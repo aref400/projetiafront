ace39e60061e76b66dd0c9370878611546ec0417
Nicolas <cazenicolas64@gmail.com>
2025-01-20 16:19:54 +0100

    tailwind marchegit add .git add .!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   theme: {
-    colors:{
+    extend: {
       colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#e86a92',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc914',
+        blue: '#1fb6ff',
+        purple: '#7e5bef',
+        pink: '#e86a92',
+        orange: '#ff7849',
+        green: '#13ce66',
+        yellow: '#ffc914',
         'gray-dark': '#273444',
-        'gray': '#8492a6',
+        gray: '#8492a6',
         'gray-light': '#d3dce6',
-      }
+      },
     },
-
-    extend: {},
   },
   plugins: [],
-}
-
+};
