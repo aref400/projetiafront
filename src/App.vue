ddad1b385bb6af5711f7ffe9d707cccf183c6fef
Lucas <lucas-lafourcade@live.com>
2025-01-21 09:54:35 +0100

    menu de ses mort

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <link href="./assets/main.css" rel="stylesheet">
-  <img alt="Vue logo" src="./assets/logo.png">
-  <nav>
-      <ul
-      class="lg:flex gap-x-5 max-lg:space-y-3 font-bold max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
-        <li class='max-lg:border-b border-gray-300 max-lg:py-3 px-3'>
-          <router-link to="/">Home</router-link></li>
-        <li><router-link to="/form">Form</router-link></li>
-        <li><router-link to="/condition">Condition</router-link></li>
+  <nav class="mx-auto flex items-center justify-around p-6 lg:px-8">
+    <div class="">
+      <img class="h-10 w-auto" alt="Vue logo" src="./assets/logo.png">
+    </div>
+  
+      <ul class="flex flex-row justify-evenly"> 
+        <li class="basis-1/3"><router-link to="/">Home</router-link></li>
+        <li class="basis-1/3"><router-link to="/form">Form</router-link></li>
+        <li class="basis-1/3"><router-link to="/condition">Condition</router-link></li>
       </ul>
-      <p class="">Ce texte devrait être rouge</p>
-    </nav>
+  </nav>
+ 
+  
     <main>
       <router-view />
     </main>
@@ -24,7 +25,11 @@ export default {
 </script>
 
 <style>
-li {
-  list-style: none;
+
+#app {
+
+    text-align: center;
+    color: var(--text-color-primary);
+    background-color: var(--background-color);
 }
 </style>
